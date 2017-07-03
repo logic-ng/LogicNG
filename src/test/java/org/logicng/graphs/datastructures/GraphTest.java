f32653b8b7dfb441eca5d5ac0c089d0807efb5be
Christoph Zengler <christoph.zengler@escsol.com>
2017-07-03 23:25:14 +0200

    merged graph content

---
+++ src/test/java/org/logicng/graphs/datastructures/GraphTest.java
@@ -0,0 +1,115 @@
+///////////////////////////////////////////////////////////////////////////
+//                   __                _      _   ________               //
+//                  / /   ____  ____ _(_)____/ | / / ____/               //
+//                 / /   / __ \/ __ `/ / ___/  |/ / / __                 //
+//                / /___/ /_/ / /_/ / / /__/ /|  / /_/ /                 //
+//               /_____/\____/\__, /_/\___/_/ |_/\____/                  //
+//                           /____/                                      //
+//                                                                       //
+//               The Next Generation Logic Library                       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+//  Copyright 2015-2016 Christoph Zengler                                //
+//                                                                       //
+//  Licensed under the Apache License, Version 2.0 (the "License");      //
+//  you may not use this file except in compliance with the License.     //
+//  You may obtain a copy of the License at                              //
+//                                                                       //
+//  http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                       //
+//  Unless required by applicable law or agreed to in writing, software  //
+//  distributed under the License is distributed on an "AS IS" BASIS,    //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      //
+//  implied.  See the License for the specific language governing        //
+//  permissions and limitations under the License.                       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+package org.logicng.graphs.datastructures;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.formulas.Variable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Unit tests for the class {@link Graph} and the class {@link Node}.
+ * @version 1.2
+ * @since 1.2
+ */
+public class GraphTest {
+
+  @Test
+  public void testLongGraph() {
+    Graph<Long> g = new Graph<Long>();
+
+    Node<Long> a = g.node(1L);
+    Node<Long> b = g.node(2L);
+    Node<Long> c = g.node(3L);
+    Node<Long> d = g.node(4L);
+    Node<Long> d2 = g.node(4L);
+
+    g.connect(d, d2);
+    Assert.assertFalse(d.neighbours().contains(d2));
+
+    g.connect(a, b);
+    g.connect(a, c);
+    g.connect(a, d);
+    g.connect(b, c);
+
+    d.connectTo(d2);
+    Assert.assertFalse(d2.neighbours().contains(d));
+
+    Assert.assertEquals(3, a.neighbours().size());
+    Assert.assertTrue(c.neighbours().contains(a));
+    Assert.assertTrue(c.neighbours().contains(b));
+
+    Assert.assertEquals("Node{content=1, neighbours:2,3,4}", a.toString());
+  }
+
+  @Test
+  public void testFormulaGraph() {
+    FormulaFactory f = new FormulaFactory();
+    Graph<Formula> g = new Graph<>();
+
+    Variable a = f.variable("A");
+    Node<Formula> an = g.node(a);
+    Variable b = f.variable("B");
+    Node<Formula> bn = g.node(b);
+
+    Formula aNb = f.and(a, b);
+    Node<Formula> aNbn = g.node(aNb);
+    g.connect(aNbn, an);
+    g.connect(aNbn, bn);
+
+    Assert.assertEquals(2, aNbn.neighbours().size());
+    Assert.assertTrue(an.neighbours().contains(aNbn));
+    Assert.assertTrue(bn.neighbours().contains(aNbn));
+
+    g.disconnect(aNbn, an);
+
+    Assert.assertTrue(an.neighbours().isEmpty());
+
+    Assert.assertEquals(3, g.nodes().size());
+  }
+
+  public static Graph<Long> getLongGraph(String id) throws IOException {
+    Graph<Long> g = new Graph<>();
+
+    final BufferedReader reader = new BufferedReader(new FileReader("tests/graphs/graph" + id + ".txt"));
+
+    while (reader.ready()) {
+      final String line = reader.readLine().trim();
+      String[] nodePair = line.split(":");
+      g.connect(g.node(Long.valueOf(nodePair[0])), g.node(Long.valueOf(nodePair[1])));
+    }
+
+    return g;
+  }
+}
---
+++ src/test/java/org/logicng/graphs/io/GraphDimacsFileWriterTest.java
@@ -0,0 +1,147 @@
+///////////////////////////////////////////////////////////////////////////
+//                   __                _      _   ________               //
+//                  / /   ____  ____ _(_)____/ | / / ____/               //
+//                 / /   / __ \/ __ `/ / ___/  |/ / / __                 //
+//                / /___/ /_/ / /_/ / / /__/ /|  / /_/ /                 //
+//               /_____/\____/\__, /_/\___/_/ |_/\____/                  //
+//                           /____/                                      //
+//                                                                       //
+//               The Next Generation Logic Library                       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+//  Copyright 2015-2016 Christoph Zengler                                //
+//                                                                       //
+//  Licensed under the Apache License, Version 2.0 (the "License");      //
+//  you may not use this file except in compliance with the License.     //
+//  You may obtain a copy of the License at                              //
+//                                                                       //
+//  http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                       //
+//  Unless required by applicable law or agreed to in writing, software  //
+//  distributed under the License is distributed on an "AS IS" BASIS,    //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      //
+//  implied.  See the License for the specific language governing        //
+//  permissions and limitations under the License.                       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+package org.logicng.graphs.io;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.Condition;
+import org.assertj.core.api.JUnitSoftAssertions;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.logicng.graphs.datastructures.Graph;
+import org.logicng.graphs.datastructures.GraphTest;
+import org.logicng.graphs.io.conditions.ContainsCondition;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Unit tests for the {@link GraphDimacsFileWriter}.
+ * @version 1.2
+ * @since 1.2
+ */
+public class GraphDimacsFileWriterTest {
+
+  @Rule
+  public final JUnitSoftAssertions softly = new JUnitSoftAssertions();
+
+  @Test
+  public void testSmall() throws IOException {
+    Graph<String> g = new Graph<>();
+    g.connect(g.node("A"), g.node("B"));
+    g.node("C");
+    testFiles("small", g);
+  }
+
+  @Test
+  public void test20() throws IOException {
+    Graph<Long> g = GraphTest.getLongGraph("30");
+    for (long i = 0; i < 30; i++) {
+      g.node(i);
+    }
+    testFiles("30", g);
+  }
+
+  @Test
+  public void test50p1() throws IOException {
+    Graph<Long> g = GraphTest.getLongGraph("50");
+    g.node(51L);
+    testFiles("50p1", g);
+  }
+
+  private void testFiles(final String fileName, final Graph g) throws IOException {
+    GraphDimacsFileWriter.write("tests/graphs/io/temp/" + fileName + "_t.col", g, true);
+    GraphDimacsFileWriter.write("tests/graphs/io/temp/" + fileName + "_f", g, false);
+    final File expectedT = new File("tests/graphs/io/graphs-dimacs/" + fileName + "_t.col");
+    final File expectedF = new File("tests/graphs/io/graphs-dimacs/" + fileName + "_f.col");
+    final File tempT = new File("tests/graphs/io/temp/" + fileName + "_t.col");
+    final File tempF = new File("tests/graphs/io/temp/" + fileName + "_f.col");
+    final File expectedMap = new File("tests/graphs/io/graphs-dimacs/" + fileName + "_t.map");
+    final File tempMap = new File("tests/graphs/io/temp/" + fileName + "_t.map");
+    assertFilesEqual(expectedT, tempT);
+    assertFilesEqual(expectedF, tempF);
+    assertMapFilesEqual(expectedMap, tempMap);
+  }
+
+  private void assertFilesEqual(final File expected, final File actual) throws IOException {
+    final BufferedReader expReader = new BufferedReader(new FileReader(expected));
+    final BufferedReader actReader = new BufferedReader(new FileReader(actual));
+    final List<String> expEdgeLines = new ArrayList<>();
+    List<String> actEdgeLines = new ArrayList<>();
+    List<String> expNodeLines = new ArrayList<>();
+    List<String> actNodeLines = new ArrayList<>();
+    for (int lineNumber = 1; expReader.ready() && actReader.ready(); lineNumber++) {
+      String exp = expReader.readLine();
+      String act = actReader.readLine();
+      if (exp.contains("{") || exp.contains("}")) {
+        softly.assertThat(act).as("Line " + lineNumber + " not equal").isEqualTo(exp);
+      } else {
+        if (exp.contains("-")) {
+          expEdgeLines.add(exp);
+        } else {
+          expNodeLines.add(exp);
+        }
+        if (act.contains("-")) {
+          actEdgeLines.add(act);
+        } else {
+          actNodeLines.add(act);
+        }
+      }
+    }
+    for (String actNode : actNodeLines) {
+      Assert.assertTrue(expNodeLines.contains(actNode));
+    }
+    for (String actEdge : actEdgeLines) {
+      String[] actEdgeNodes = actEdge.trim().split(" ");
+
+      Condition<? super List<? extends String>> left = new ContainsCondition(actEdge);
+      Condition<? super List<? extends String>> right = new ContainsCondition("e " + actEdgeNodes[2] + " " + actEdgeNodes[1]);
+      softly.assertThat(expEdgeLines).has(Assertions.anyOf(left, right));
+    }
+    if (expReader.ready())
+      softly.fail("Missing line(s) found, starting with \"" + expReader.readLine() + "\"");
+    if (actReader.ready())
+      softly.fail("Additional line(s) found, starting with \"" + actReader.readLine() + "\"");
+  }
+
+  private void assertMapFilesEqual(final File expected, final File actual) throws IOException {
+    final BufferedReader expReader = new BufferedReader(new FileReader(expected));
+    final BufferedReader actReader = new BufferedReader(new FileReader(actual));
+    for (int lineNumber = 1; expReader.ready() && actReader.ready(); lineNumber++)
+      softly.assertThat(actReader.readLine()).as("Line " + lineNumber + " not equal").isEqualTo(expReader.readLine());
+    if (expReader.ready())
+      softly.fail("Missing line(s) found, starting with \"" + expReader.readLine() + "\"");
+    if (actReader.ready())
+      softly.fail("Additional line(s) found, starting with \"" + actReader.readLine() + "\"");
+  }
+}
