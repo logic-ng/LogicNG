f7083d3bb7236cd97dc1923a24a0d3bd56f95a00
Christoph Zengler <christoph.zengler@escsol.com>
2018-04-19 21:22:22 +0200

    begin QMC implementation

---
+++ src/main/java/org/logicng/transformations/qmc/Term.java
@@ -0,0 +1,146 @@
+///////////////////////////////////////////////////////////////////////////
+//                   __                _      _   ________               //
+//                  / /   ____  ____ _(_)____/ | / / ____/               //
+//                 / /   / __ \/ __ `/ / ___/  |/ / / __                 //
+//                / /___/ /_/ / /_/ / / /__/ /|  / /_/ /                 //
+//               /_____/\____/\__, /_/\___/_/ |_/\____/                  //
+//                           /____/                                      //
+//                                                                       //
+//               The Next Generation Logic Library                       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+//                                                                       //
+//  Copyright 2015-2018 Christoph Zengler                                //
+//                                                                       //
+//  Licensed under the Apache License, Version 2.0 (the "License");      //
+//  you may not use this file except in compliance with the License.     //
+//  You may obtain a copy of the License at                              //
+//                                                                       //
+//  http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                       //
+//  Unless required by applicable law or agreed to in writing, software  //
+//  distributed under the License is distributed on an "AS IS" BASIS,    //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      //
+//  implied.  See the License for the specific language governing        //
+//  permissions and limitations under the License.                       //
+//                                                                       //
+///////////////////////////////////////////////////////////////////////////
+
+package org.logicng.transformations.qmc;
+
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.Formula;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * A term in the Quine–McCluskey algorithm.  A term represents a set of
+ * minterms of a canonical DNF and has its own representation as a vector
+ * of tristates.
+ * @version 1.4.0
+ * @since 1.4.0
+ */
+public class Term {
+
+  private final Tristate[] bits;
+  private final List<Formula> minterms;
+  private final int termClass;
+  private boolean used;
+
+  /**
+   * Constructs a new term with a given set of bits and the related minterms.
+   * @param bits     the bits
+   * @param minterms the minterms
+   */
+  Term(final Tristate[] bits, final List<Formula> minterms) {
+    this.bits = bits;
+    this.minterms = minterms;
+    this.termClass = countNonNegativeBits(bits);
+  }
+
+  /**
+   * Counts the number of non-negative bits of a given tristate vector.
+   * @param bits the tristate vector
+   * @return the number of non-negative bits
+   */
+  private int countNonNegativeBits(final Tristate[] bits) {
+    int result = 0;
+    for (final Tristate bit : bits)
+      if (bit != Tristate.FALSE)
+        result++;
+    return result;
+  }
+
+  public Tristate[] bits() {
+    return this.bits;
+  }
+
+  public List<Formula> minterms() {
+    return this.minterms;
+  }
+
+  public int termClass() {
+    return this.termClass;
+  }
+
+  public boolean isUsed() {
+    return this.used;
+  }
+
+  public void setUsed(final boolean used) {
+    this.used = used;
+  }
+
+  /**
+   * Units this term with another term if possible.  This is only possible if their bit vectors
+   * differ in exactly one position.  In this case a new term with the new bit vector and the
+   * united minterms is returned.  If no union is possible, {@code null} is returned.
+   * @param other the other term
+   * @return a new united term or {@code null} if not possible
+   */
+  Term unite(final Term other) {
+    if (this.bits.length != other.bits.length)
+      return null;
+    int diffPosition = -1;
+    for (int i = 0; i < this.bits.length; i++)
+      if (this.bits[i] != other.bits[i]) {
+        if (diffPosition != -1)
+          return null;
+        else
+          diffPosition = i;
+      }
+    if (diffPosition == -1)
+      return null;
+    final Tristate[] newBits = Arrays.copyOf(this.bits, this.bits.length);
+    newBits[diffPosition] = Tristate.UNDEF;
+    final List<Formula> newMinterms = new ArrayList<>(this.minterms);
+    newMinterms.addAll(other.minterms);
+    return new Term(newBits, newMinterms);
+  }
+
+  @Override
+  public boolean equals(final Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    final Term term = (Term) o;
+    return Arrays.equals(this.bits, term.bits) &&
+            Objects.equals(this.minterms, term.minterms);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(this.bits, this.minterms);
+  }
+
+  @Override
+  public String toString() {
+    return "Term{" +
+            "bits=" + Arrays.toString(this.bits) +
+            ", minterms=" + this.minterms +
+            ", termClass=" + this.termClass +
+            '}';
+  }
+}
---
+++ src/test/java/org/logicng/transformations/qmc/TermTest.java
@@ -0,0 +1,57 @@
+package org.logicng.transformations.qmc;
+
+import org.junit.Test;
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.logicng.datastructures.Tristate.FALSE;
+import static org.logicng.datastructures.Tristate.TRUE;
+import static org.logicng.datastructures.Tristate.UNDEF;
+
+/**
+ * Unit tests for {@link Term}.
+ * @version 1.4.0
+ * @since 1.4.0
+ */
+public class TermTest {
+
+  @Test
+  public void testUnite() {
+    final Tristate[] bits1 = new Tristate[]{TRUE, TRUE, TRUE, TRUE};
+    final Tristate[] bits2 = new Tristate[]{TRUE, FALSE, TRUE, TRUE};
+    final Tristate[] bits3 = new Tristate[]{TRUE, TRUE, FALSE, TRUE};
+    final Tristate[] bits4 = new Tristate[]{FALSE, FALSE, TRUE, TRUE};
+    final Tristate[] bits5 = new Tristate[]{TRUE, FALSE, FALSE, UNDEF};
+    final Tristate[] bits6 = new Tristate[]{TRUE, FALSE, TRUE, UNDEF};
+
+    final FormulaFactory f = new FormulaFactory();
+    final List<Formula> formulas1 = Collections.<Formula>singletonList(f.variable("f1"));
+    final List<Formula> formulas2 = Collections.<Formula>singletonList(f.variable("f2"));
+    final List<Formula> formulas3 = Collections.<Formula>singletonList(f.variable("f3"));
+    final List<Formula> formulas4 = Collections.<Formula>singletonList(f.variable("f4"));
+    final List<Formula> formulas5 = Collections.<Formula>singletonList(f.variable("f5"));
+    final List<Formula> formulas6 = Collections.<Formula>singletonList(f.variable("f6"));
+
+    final Term term1 = new Term(bits1, formulas1);
+    final Term term2 = new Term(bits2, formulas2);
+    final Term term3 = new Term(bits3, formulas3);
+    final Term term4 = new Term(bits4, formulas4);
+    final Term term5 = new Term(bits5, formulas5);
+    final Term term6 = new Term(bits6, formulas6);
+
+    assertThat(term1.unite(term2)).isEqualTo(new Term(new Tristate[]{TRUE, UNDEF, TRUE, TRUE}, Arrays.<Formula>asList(f.variable("f1"), f.variable("f2"))));
+    assertThat(term1.unite(term3)).isEqualTo(new Term(new Tristate[]{TRUE, TRUE, UNDEF, TRUE}, Arrays.<Formula>asList(f.variable("f1"), f.variable("f3"))));
+    assertThat(term2.unite(term3)).isNull();
+    assertThat(term1.unite(term4)).isNull();
+    assertThat(term2.unite(term4)).isEqualTo(new Term(new Tristate[]{UNDEF, FALSE, TRUE, TRUE}, Arrays.<Formula>asList(f.variable("f2"), f.variable("f4"))));
+    assertThat(term3.unite(term4)).isNull();
+    assertThat(term5.unite(term6)).isEqualTo(new Term(new Tristate[]{TRUE, FALSE, UNDEF, UNDEF}, Arrays.<Formula>asList(f.variable("f5"), f.variable("f6"))));
+  }
+
+}
