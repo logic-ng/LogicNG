43c5a28ae20f2b612a422de9451e1f0244c4904f
Christoph Zengler <christoph.zengler@booleworks.com>
2019-03-11 16:54:29 +0100

    large CNF for subsumption test

--- src/test/java/org/logicng/transformations/cnf/CNFSubsumptionTest.java
+++ src/test/java/org/logicng/transformations/cnf/CNFSubsumptionTest.java
@@ -1,11 +1,23 @@
 package org.logicng.transformations.cnf;
 
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.logicng.formulas.Formula;
 import org.logicng.formulas.FormulaFactory;
 import org.logicng.io.parsers.ParserException;
 import org.logicng.io.parsers.PropositionalParser;
+import org.logicng.io.readers.FormulaReader;
+import org.logicng.predicates.satisfiability.TautologyPredicate;
 
+import java.io.IOException;
+
+/**
+ * Unit tests for {@link CNFSubsumption}.
+ * @version 1.5.0
+ * @since 1.5.0
+ */
 public class CNFSubsumptionTest {
 
     private final FormulaFactory f = new FormulaFactory();
@@ -40,5 +52,15 @@ public class CNFSubsumptionTest {
         Assert.assertEquals(this.s.apply(this.p.parse("(a | b) & (a | c) & (a | b | c) & (a | ~b | c) & (a | b | ~c) & (b | c)"), false), this.p.parse("(a | b) & (a | c) & (b | c)"));
         Assert.assertEquals(this.s.apply(this.p.parse("a & ~b & (c | d) & (~a | ~b | ~c) & (b | c | d) & (a | b | c | d)"), false), this.p.parse("a & ~b & (c | d)"));
         Assert.assertEquals(this.s.apply(this.p.parse("(a | b | c | d | e | f | g) & (b | d | f) & (a | c | e | g)"), false), this.p.parse("(b | d | f) & (a | c | e | g)"));
+    }
+
+    @Test
+    public void testEvenLargerFormula() throws IOException, ParserException {
+        final FormulaFactory f = new FormulaFactory();
+        final Formula formula = FormulaReader.readPseudoBooleanFormula("src/test/resources/formulas/large_formula.txt", f);
+        final Formula cnf = formula.transform(new CNFFactorization());
+        final Formula subsumed = cnf.transform(new CNFSubsumption());
+        assertThat(f.equivalence(cnf, subsumed).holds(new TautologyPredicate(f))).isTrue();
+        assertThat(cnf.numberOfOperands() > subsumed.numberOfOperands()).isTrue();
     }
 }
