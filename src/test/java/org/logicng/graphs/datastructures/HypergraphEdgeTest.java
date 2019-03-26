284b3aa8458d2369fde3ce3c8b4c4b462adf235d
Rouven Walter <rouven.walter@booleworks.com>
2019-03-26 23:58:46 +0100

    adjusted copyright header

--- src/test/java/org/logicng/graphs/datastructures/HypergraphEdgeTest.java
+++ src/test/java/org/logicng/graphs/datastructures/HypergraphEdgeTest.java
@@ -10,7 +10,7 @@
 //                                                                       //
 ///////////////////////////////////////////////////////////////////////////
 //                                                                       //
-//  Copyright 2015-2016 Christoph Zengler                                //
+//  Copyright 2015-20xx Christoph Zengler                                //
 //                                                                       //
 //  Licensed under the Apache License, Version 2.0 (the "License");      //
 //  you may not use this file except in compliance with the License.     //
